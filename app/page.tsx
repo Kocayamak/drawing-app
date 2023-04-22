e912c652ff83ea7618aa93d73faa70e5a7a99c89
Kocayamak <mikailkocayamak@gmail.com>
2023-04-22 21:23:22 +0300

    draw and points types created.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+'use client'
+
+import { useDraw } from "@/hooks/useDraw";
 import { FC } from "react";
 
 interface PageProps {}
 
 const Page: FC<PageProps> = ({}) => {
+  const { canvasRef } = useDraw(drawLine);
+
+  function drawLine({ prevPoint, currentPoint, ctx } : Draw){
+
+  }
+
   return (
     <div className="w-screen h-screen bg-white flex justify-center items-center">
-      <canvas width={750} height={750} className="border border-black rounded-md" />
+      <canvas ref={canvasRef} width={750} height={750} className="border border-black rounded-md" />
 
     </div>
   );
